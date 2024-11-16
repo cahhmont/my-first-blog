b619251f826cfb6185c62701ad581814c05ae6da
Carolina Monteiro <cahh-monteiro99@hotmail.com>
2024-11-16 12:17:41 -0300

    My Django Girls app, first commit

---
+++ script.py
@@ -0,0 +1,20 @@
+print('Hello, django girls')
+if 3 > 2:
+    print('Funciona!')
+if 5 < 3:
+    print('3 é maior que 5')
+else:
+    print('5 é maior que 3')
+def hi():
+    print('Olá!')
+    print('Tudo bem?')
+def hi(name):
+    print('Olá ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'você']
+for name in girls:
+    hi(name)
+    print('Próxima')
+for i in range(1, 6):
+    print(i)
+    
